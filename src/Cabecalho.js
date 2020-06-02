3c11e4342099216a527f27831a38d972662c0249
Luan Oliveira Santos <luanoliveirasantos@hotmail.com>
2020-06-01 21:28:41 -0300

    Finalização do curriculum feito em react

---
+++ src/Cabecalho.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Cabecalho() {
+  return (
+    <div id="header">
+      <div>
+        <h1>Luan Oliveira Santos</h1>
+        <p>Brasileiro, 17 anos, solteiro</p>
+        <p>Telefone: (11) 99536-8724</p>
+        <p>
+          Email: <a href="mailto:exemplo@email.com">exemplo@email.com</a>
+        </p>
+        <p>Endereço: Rua do Oratório Nº378, Mooca, São Paulo - SP</p>
+      </div>
+    </div>
+  );
+}
+export default Cabecalho;
---
+++ src/Conteudo.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+function Conteudo() {
+  return (
+    <div>
+      <div id="titulo-conteudo">
+        <h2>Objetivo</h2>
+      </div>
+      <div id="lista-conteudo">
+        <ul>
+          <li>
+            Adquirir experiência na área de tecnologia da informação, com o
+            intuito de expandir conhecimento e conseguir êxito em todos os
+            trabalhos e projetos ao qual eu for incluido{" "}
+          </li>
+        </ul>
+      </div>
+      <div id="titulo-conteudo">
+        <h2>Formação</h2>
+      </div>
+      <div id="lista-conteudo">
+        <ul>
+          <li>
+            Cursando Técnico em informática no Senac Tatuapé - Com previsão de
+            término em Outubro de 2020
+          </li>
+          <li>
+            Ensino Médio(Completo) - Escola: ETEC Professor Camargo Aranha{" "}
+          </li>
+        </ul>
+      </div>
+      <div id="titulo-conteudo">
+        <h2>Conhecimento</h2>
+      </div>
+      <div id="lista-conteudo">
+        <ul>
+          <li>Redes: Manutencão, Instalção</li>
+          <li>Hardware: Manutencão, Instalção e Montagem</li>
+          <li>Banco de dados: MySQL, Tomcat e Apache</li>
+          <li>
+            Desenvolvimento Web: HTML, CSS, JavaScript, Jquery, Bootstrap,
+            Materialize e React
+          </li>
+        </ul>
+      </div>
+      <div id="titulo-conteudo">
+        <h2>Idiomas</h2>
+      </div>
+      <div id="lista-conteudo">
+        <ul>
+          <li>Português - Fluente</li>
+          <li>Inglês - Básico</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default Conteudo;
